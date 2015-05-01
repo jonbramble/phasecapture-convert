b4f9d1723c36d88f50d4abf8a408e2e316489e11
Jonathan Bramble <jbramble82@gmail.com>
2015-05-01 14:33:26 +0100

    added gpl3 headings

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -4,17 +4,30 @@
  *  Created on: 29 Apr 2015
  *      Author: mbajb
  */
+/*
+PhaseCapture Convert - HDF5 file format for PhaseCapture Camera Data
+    Copyright (C) 2015  Jonathan Bramble
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "Frame.h"
 
 Frame::Frame() {
-	// TODO Auto-generated constructor stub
-	//std::cout << "Frame Constructor called " << std::endl;
 	frame_data.resize(IMAGE::FRAME_SIZE);
-
 }
 
 Frame::~Frame() {
-	// TODO Auto-generated destructor stub
 }
 
